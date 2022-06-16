87aa72b01e4aac528fd76862c05c931d09d5afdf
Fael <91755583+justmefael@users.noreply.github.com>
2022-06-16 16:59:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const { warn, client } = require('./function.js')
-warn("Pode demorar entre 1~2 minutos para fazer conexão com usuário")
+warn("It may take between 1~2 minutes to connect with user")
 
 client(1)
